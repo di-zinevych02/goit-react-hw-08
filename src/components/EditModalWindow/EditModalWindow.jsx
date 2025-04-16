79ba3b220ba2ad3107204b198eef93705543396c
Diana Zinevych <dianazinevych02@gmail.com>
2025-04-16 23:50:21 +0200

    add validate in edit form

--- src/components/EditModalWindow/EditModalWindow.jsx
+++ src/components/EditModalWindow/EditModalWindow.jsx
@@ -1,6 +1,10 @@
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from "yup";
 import css from './EditModalWindow.module.css';
-
+const ContactSchema = Yup.object().shape({
+    name: Yup.string().min(3, "Too Short!").max(50, "Too Long!").required("Required"),
+    number: Yup.string().min(3, "Too Short!").max(50, "Too Long!").required("Required").matches(/^\d{3}-\d{2}-\d{2}$/, 'Number Format: 123-45-67'),
+});
 const EditModalWindow = ({ isOpen, onClose, contact, onSubmit }) => {
   if (!isOpen) return null;
 
@@ -10,7 +14,8 @@ const EditModalWindow = ({ isOpen, onClose, contact, onSubmit }) => {
         <h3>Edit Contact</h3>
         <Formik
           initialValues={{ name: contact.name, number: contact.number }}
-          onSubmit={(values) => {
+          validationSchema={ContactSchema}
+                  onSubmit={(values) => {
             onSubmit(contact.id, values);
             onClose();
           }}
@@ -18,12 +23,16 @@ const EditModalWindow = ({ isOpen, onClose, contact, onSubmit }) => {
           <Form className={css.form}>
             <label>
               Name:
-              <Field name="name" />
-            </label>
+                          <Field name="name" />
+                          <ErrorMessage className={css.error} name="name" component="span"/>
+                      </label>
+                      
             <label>
               Number:
               <Field name="number" />
-            </label>
+                     <ErrorMessage className={css.error} name="number" component="span"/>
+                      </label>
+                       
             <div className={css.btnWrap}>
               <button type="submit">Save</button>
               <button type="button" onClick={onClose}>Cancel</button>
